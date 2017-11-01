aa9cf36caabdb641bf77663ff6010ea68f555bef
Ahmed Alaa Hagag <ahmedalaahagag@gmail.com>
2017-11-01 13:37:18 +0200

    Update Convertertest.php

--- tests/Convertertest.php
+++ tests/Convertertest.php
@@ -6,17 +6,7 @@ require_once '../src/File/CSV/Converter.php';
 final class Convertertest extends TestCase{
     public function testConstructorHasStrategyPassedToIt()
     {
-        $strategy = new File_CSV_Converter_Strategy_ToHTML();
-        $this->assertInstanceOf(
-            File_CSV_Converter_Strategy::class,
-            $strategy);
-        $strategy = new File_CSV_Converter_Strategy_ToJSON();
-        $this->assertInstanceOf(
-            File_CSV_Converter_Strategy::class,
-            $strategy);
-        $map = ['name','stars','address','uri'];
-        $this->assertType('array', $map);
-        $convertor = new File_CSV_Converter($map,$strategy);
+       
     }
 
 }
